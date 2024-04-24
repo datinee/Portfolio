8b5474b8d485922199d005ac369c269585e8c8c6
datinee <145119510+datinee@users.noreply.github.com>
2024-04-24 12:18:30 +0200

    NEW

--- SiteEceframStagiaires/Cookies_refuser.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!isset($_COOKIE['accept_cookies']) and empty($_COOKIE['accept_cookies'])) {
-    // header('Location:index.php');
-    header('Location:' . $_SERVER['HTTP_SERVER']);
-}
--- SiteEceframStagiaires/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//constante de connexion
-define("HOTE", 'localhost:3306');
-define("BDD", 'ecefram_BD');
-define("UTILISATEUR", 'root');
-define("MDP", '');
